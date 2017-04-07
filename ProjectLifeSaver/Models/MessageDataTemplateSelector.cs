d6312f8678c6b2cdb267fdcda88ec9a170867923
David Knieradl <fd45ea11@gmail.com>
2017-04-08 01:20:48 +0200

    WHY IS IT NOT WORKING BOB, WHY IS IT NOT WORKIN

---
+++ ProjectLifeSaver/Models/MessageDataTemplateSelector.cs
@@ -0,0 +1,23 @@
+﻿using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+
+namespace ProjectLifeSaver.Models
+{
+    public class MessageDataTemplateSelector : DataTemplateSelector
+    {
+        public DataTemplate Assistant { get; set; }
+        public DataTemplate Me { get; set; }
+
+        protected override DataTemplate SelectTemplateCore(object item,
+                                                       DependencyObject container)
+        {
+            if (((MessageData)item).Sender == "assistant")
+                return Assistant;
+
+            if (((MessageData)item).Sender == "me")
+                return Me;
+
+            return base.SelectTemplateCore(item, container);
+        }
+    }
+}
