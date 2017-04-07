a265d72461642b3007875614bf877532149f77c3
David Knieradl <fd45ea11@gmail.com>
2017-04-07 18:02:38 +0200

    Almost done UI for profile page

---
+++ ProjectLifeSaver/Models/Blood.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjectLifeSaver.Models
+{
+    public static class Blood
+    {
+        public enum BloodType
+        {
+            APositive,
+            ANegative,
+            ANull,
+
+            BPositive,
+            BNegative,
+            BNull,
+
+            ABPositive,
+            ABNegative,
+            ABNull,
+
+            OPositive,
+            ONegative,
+            ONull,
+        }
+    }
+}
---
+++ ProjectLifeSaver/Models/Profile.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static ProjectLifeSaver.Models.Blood;
+
+namespace ProjectLifeSaver.Models
+{
+    class Profile
+    {
+        public string Name { get; set; }
+        public byte Age { get; set; }
+        public BloodType Blood { get; set; }
+        public string Description { get; set; }
+        public ObservableCollection<Disease> Diseases { get; }
+
+        public Profile(string Name, byte Age, BloodType Blood, string Description)
+        {
+            this.Name = Name;
+            this.Age = Age;
+            this.Blood = Blood;
+            this.Description = Description;
+        }
+
+        public void AddDisease(string Name, string Description) => Diseases.Add(new Disease(Name, Description));
+    }
+}
