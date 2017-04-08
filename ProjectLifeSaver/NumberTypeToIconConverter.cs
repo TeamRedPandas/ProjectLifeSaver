cdab364f9c8e9ed726d7f2331126600fe771ee1b
David Knieradl <fd45ea11@gmail.com>
2017-04-08 10:53:37 +0200

    Added PhoneBook data

---
+++ ProjectLifeSaver/NumberTypeToIconConverter.cs
@@ -0,0 +1,27 @@
+﻿using ProjectLifeSaver.Models;
+using System;
+using Windows.UI.Xaml.Data;
+
+namespace ProjectLifeSaver
+{
+    public sealed class NumberTypeToIconConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, string language)
+        {
+            switch ((Number.Types)value)
+            {
+                case Number.Types.Police:
+                case Number.Types.Ambulance:
+                case Number.Types.FireDept:
+                case Number.Types.Sos:
+                    return "ms-appx:Assets/" + value.ToString() + ".png";
+                default: throw new Exception(@"You're not doing it right ¯\_ツ_/¯");
+            }
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, string language)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
